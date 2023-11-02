60cf175e7b1d7c9ae3b3c03a4f3ff61b30269dc8
Hakan KARAYILMAZ <hkankyilmazz@gmail.com>
2023-11-03 01:06:30 +0300

    homework 3 part1 completed

--- Homeworks/HOMEWORK_3_POSTGRESQL.sql
+++ Homeworks/HOMEWORK_3_POSTGRESQL.sql
@@ -65,7 +65,15 @@ where f.length <60
 
 --8
 
-
+select c.name , count(r.rental_id)
+from film f join film_category fc 
+on f.film_id = fc.film_id join inventory i 
+on i.film_id =f.film_id join rental r 
+on r.rental_id =i.inventory_id  join category c 
+on fc.category_id =c.category_id 
+group by c."name" 
+order by count(r.rental_id) desc
+limit 10
 
 
 
